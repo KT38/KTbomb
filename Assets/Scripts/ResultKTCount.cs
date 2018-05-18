73f021e427d23eab55608c2d7499790e26ff3c43
tenmakun <hyper.tenmakun@docomo.ne.jp>
2018-05-18 13:01:57 +0900

    throw

---
+++ Assets/Scripts/ResultKTCount.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultKTCount : MonoBehaviour {
+	public UnityEngine.UI.Text ResultCount;
+
+	// Update is called once per frame
+	void Update () {
+		int resultcount = CountKT.count;
+		ResultCount.text = "Score:" + resultcount.ToString ();
+	}
+}
